2217262de17d95d1abfa23b78f27d599b26c5850
Vitali Griunvald <vgriunvald@gmail.com>
2021-06-22 21:48:51 +0300

    Render data from array as a player data on app init

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,24 @@
-const songsList = fetch('http://localhost:3000/songs_list.json')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+// const songsList = fetch('http://localhost:3000/songs_list.json')
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+// console.log(songsList);
+
+const songsList = [
+  {
+    artist: 'Led Zeppelin',
+    title: 'Good Times Bad Times',
+    year: '1969',
+    cover: 'images/led_zeppelin.png',
+    file: 'songs/1.mp3',
+  },
+];
+
+const songIndex = 0;
+const currentSong = songsList[songIndex];
+
+const initSong = () => {
+  artist.innerText = currentSong.artist;
+  title.innerText = currentSong.title;
+  year.innerText = currentSong.year;
+};
+initSong();
