422fa733d81745b13246a9a293630092080ae388
Vitali Griunvald <vgriunvald@gmail.com>
2021-06-23 01:48:27 +0300

    Fetch data from server, populate in the app

--- public/js/app.js
+++ public/js/app.js
@@ -1,62 +1,44 @@
-// const songsList = fetch('http://localhost:3000/songs_list.json')
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
-// console.log(songsList);
+let songsList;
 
-const songsList = [
-  {
-    artist: 'Led Zeppelin',
-    title: 'Good Times Bad Times',
-    year: '1969',
-    cover: 'images/led_zeppelin.png',
-    file: 'songs/1.mp3',
-  },
-  {
-    artist: 'The Rolling Stones',
-    title: 'Gimmi Shelter',
-    year: '1969',
-    cover: 'images/rolling_stones.jpg',
-    file: 'songs/2.mp3',
-  },
-  {
-    artist: 'Jimy Hendrix',
-    title: 'Foxy Lady',
-    year: '1967',
-    cover: 'images/jimy_hendrix.jpg',
-    file: 'songs/3.mp3',
-  },
-];
+fetch('http://localhost:3000/songs_list.json')
+  .then((response) => response.json())
+  .then((data) => {
+    songsList = data;
+    onSongsReturn(songsList);
+  });
 
-let songIndex = 2;
-let currentSong = songsList[songIndex];
-console.log(currentSong);
-
-const initSong = () => {
-  album_cover.src = currentSong.cover;
-  artist.innerText = currentSong.artist;
-  title.innerText = currentSong.title;
-  year.innerText = currentSong.year;
-  audio.src = currentSong.file;
-};
-initSong();
-
-play.addEventListener('click', () => {
-  const playButton = document.getElementById('play');
-  isPlaying = playButton.classList.contains('fa-pause');
-  console.log(isPlaying);
-  if (isPlaying) {
-    playButton.classList.remove('fa-pause');
-    audio.pause();
-    playButton.classList.add('fa-play');
-  } else if (!isPlaying) {
-    audio.play();
-    playButton.classList.add('fa-pause');
-  }
-});
-
-forward.addEventListener('click', () => {
-  songIndex++;
-  initSong();
-  console.log(songIndex);
+const onSongsReturn = (songsList) => {
+  let songIndex = 2;
+  let currentSong = songsList[songIndex];
   console.log(currentSong);
-});
+
+  const initSong = () => {
+    album_cover.src = currentSong.cover;
+    artist.innerText = currentSong.artist;
+    title.innerText = currentSong.title;
+    year.innerText = currentSong.year;
+    audio.src = currentSong.file;
+  };
+  initSong();
+
+  play.addEventListener('click', () => {
+    const playButton = document.getElementById('play');
+    isPlaying = playButton.classList.contains('fa-pause');
+    console.log(isPlaying);
+    if (isPlaying) {
+      playButton.classList.remove('fa-pause');
+      audio.pause();
+      playButton.classList.add('fa-play');
+    } else if (!isPlaying) {
+      audio.play();
+      playButton.classList.add('fa-pause');
+    }
+  });
+
+  forward.addEventListener('click', () => {
+    songIndex++;
+    initSong();
+    console.log(songIndex);
+    console.log(currentSong);
+  });
+};
